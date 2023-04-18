0b3523bb6fb2e17dd90c145daac18725b53441cf
Matt Marino <matt.marino@shopify.com>
2023-04-18 10:24:07 -0400

    initial commit

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+
+const FeedbackData = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'The highest rating congrats!',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Silver medal good job.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'Bronze, better luck next time.',
+    },
+  ]
+
+  export default FeedbackData;
